81397a5e26c1b6f6c650e0f81eddcdbeec6b15a2
qiuxan <qiuxan@gmail.com>
2026-05-22 19:32:53 +1000

    Connect Front End and Backend

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -10,7 +10,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req: Request, res: Response) => {
-  res.send(process.env.OPENAI_API_KEY || "Hello, World!");
+  res.send( "Hello, World!");
+});
+
+app.get("/api/hello", (req: Request, res: Response) => {
+  res.json({ message: "Hello, World!" });
 });
 
 app.listen(port, () => {
