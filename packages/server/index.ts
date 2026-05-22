484551cf31c0a7a5f7d9cfacc8ac1298c1444840
qiuxan <qiuxan@gmail.com>
2026-05-22 18:33:05 +1000

    Manage API key in .env file

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,12 +1,16 @@
 import express from "express";
 import type { Request, Response } from "express";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello world!");
+  res.send(process.env.OPENAI_API_KEY || "Hello, World!");
 });
 
 app.listen(port, () => {
