6a2d387c0bf65963edace10f6c48c4c1887250e1
qiuxan <qiuxan@gmail.com>
2026-05-23 20:06:42 +1000

    BE: Introdcue OpenAI and create the first chat API

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,10 +1,25 @@
 import express from 'express';
 import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
+import OpenAI from 'openai';
 
 dotenv.config();
 
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+
+if (!OPENAI_API_KEY) {
+   console.error(
+      'Error: OPENAI_API_KEY is not set in the environment variables.'
+   );
+   process.exit(1);
+}
+
+const client = new OpenAI({
+   apiKey: OPENAI_API_KEY,
+});
+
 const app = express();
+app.use(express.json());
 
 const port = process.env.PORT || 3000;
 
@@ -14,6 +29,19 @@ app.get('/', (req: Request, res: Response) => {
 
 app.get('/api/hello', (req: Request, res: Response) => {
    res.json({ message: 'Hello, World!' });
+});
+
+app.post('/api/chat', express.json(), async (req: Request, res: Response) => {
+   const { prompt } = req.body;
+
+   const response = await client.responses.create({
+      model: 'gpt-4o-mini',
+      input: prompt,
+      temperature: 0.2,
+      max_output_tokens: 100,
+   });
+
+   res.json({ response: response.output_text });
 });
 
 app.listen(port, () => {
